707bd43d55d38571f3f8ed65be79cf1cf992a7f1
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-06-19 21:51:09 +0100

    kinda finished
    finished but needs to be polished. A LOT.

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ function divide(num1, num2){
 }
 
 function operate(num1, operator, num2){
+    if (operator == "/" && (num2 == 0 || num1 == 0)){
+        return "Can't divide by 0";
+    }
     if (operator == "+"){
         return add(num1, num2);
     }else if (operator == "-"){
@@ -36,11 +39,18 @@ function operate(num1, operator, num2){
     }
 }
 
+const display = document.querySelector(".display");
+const defaultDisplay = "0000000000";
+
+function updateDisplay(num){
+    display.textContent = num;
+}
+
 const buttoncontainer = document.querySelector(".buttons-container");
 let span = "";
-let num1, num2 = 0;
+let num1 = -1, num2 = -1;
 let operator = "";
-let clickedOperator = false, num1IsSet = false;
+let clickedOperator = false, num1IsSet = false, clickedEquals = false;
 buttoncontainer.addEventListener("click", (e) => {
     const isButton = e.target.nodeName === 'INPUT';
     if (!isButton){
@@ -48,8 +58,13 @@ buttoncontainer.addEventListener("click", (e) => {
     }
 
     if((parseInt(e.target.value) >= 0 && parseInt(e.target.value) <= 9) && num1IsSet == false) {
-        span += e.target.value;
+        if(clickedEquals == true){
+            span = e.target.value;
+        }else{
+            span += e.target.value;
+        }
         console.log(span);
+        updateDisplay(span);
     }
     if(e.target.value == "+" || e.target.value == "-" || e.target.value == "*" || e.target.value == "/") {
         if (clickedOperator == false){
@@ -57,24 +72,44 @@ buttoncontainer.addEventListener("click", (e) => {
             span = e.target.value;
             operator = span;
             console.log(span);
+            updateDisplay(span);
             clickedOperator = true;
             num1IsSet = true;
             span = "";
-        } else if (clickedOperator == true){
+        } else if (clickedOperator == true && num2 >= 0){
             console.log("only two operands pls");
+        } else if (clickedOperator == true && num2 < 0){
+            span = e.target.value;
+            operator = span;
+            updateDisplay(span);
+            span = "";
         }
     }
     if((parseInt(e.target.value) >= 0 && parseInt(e.target.value) <= 9) && num1IsSet == true) {
-        span += e.target.value;
+        if(clickedEquals == true){
+            span = e.target.value;
+        }else{
+            span += e.target.value;
+        }
         console.log(span);
+        updateDisplay(span);
     }
     if(e.target.value == "="){
+        if (operator == ""){
+            span = "Please enter an operation!";
+            updateDisplay(span);
+            span = "";
+            return ;
+        }
         num2 = parseInt(span);
         span = operate(num1, operator, num2);
         console.log(span);
+        updateDisplay(span);
         num1 = parseInt(span);
-        num1IsSet == true;
+        num2 = -1;
+        num1IsSet = true;
         clickedOperator = false;
+        clickedEquals = true;
     }
     if(e.target.value == "clear"){
         num1 = 0;
@@ -83,6 +118,8 @@ buttoncontainer.addEventListener("click", (e) => {
         operator = "";
         num1IsSet = false;
         clickedOperator = false;
+        clickedEquals = false;
+        updateDisplay(defaultDisplay);
     }
 })
 
